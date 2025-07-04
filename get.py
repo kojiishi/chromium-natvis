8fe94eb91f0b7a01e5124bc150d1033d7d738565
Koji Ishii <kojii@chromium.org>
2025-07-04 18:19:32 +0900

    Sync 2025/7/4

---
+++ get.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env -S uv run --script
+#
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#   "platformdirs",
+# ]
+# ///
+
+import argparse
+import glob
+import logging
+import os
+from pathlib import Path
+import shutil
+from typing import Iterable, Iterator
+
+import platformdirs
+
+class VsNatVis(object):
+    def __init__(self):
+        pass
+    
+    def get(self):
+        src_dir = VsNatVis.chromium_natvis_dir()
+        VsNatVis.copy(src_dir, list(VsNatVis.user_dirs()))
+    
+    @staticmethod
+    def copy(src_dir: Path, dst_dirs: Iterable[Path]):
+        for file in src_dir.iterdir():
+            name = file.name
+            if name.endswith(".natvis") or name.endswith(".natstepfilter"):
+                logging.info(f"Copying {file.name} in {file.parent}")
+                for dst_dir in dst_dirs:
+                    logging.info(f"Copying -> {dst_dir}")
+                    shutil.copy(file, dst_dir)
+
+    @staticmethod
+    def chromium_natvis_dir() -> Path:
+        chromium_src = Path(os.environ["CHROMIUM_SRC"])
+        return chromium_src / "tools" / "win" / "DebugVisualizers"
+
+    @staticmethod
+    def user_dirs() -> Iterator[Path]:
+        doc_dir = Path(platformdirs.user_documents_dir())
+        user_vs_dirs = glob.glob(str(doc_dir / "Visual Studio *"))
+        user_dirs = map(lambda dir: Path(dir) / "Visualizers", user_vs_dirs)
+        return user_dirs
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("args", nargs="*")
+    args = parser.parse_args()
+    natvis = VsNatVis()
+    if len(args.args) == 0:
+        natvis.get()
+        return
+    for arg in args.args:
+        if "get".startswith(arg):
+            natvis.get()
+
+if __name__ == "__main__":
+    main()
